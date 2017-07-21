9c1f345c3e2fe73e6544a534d9253acc8b9843a4
Jéfferson Soares <jefferson.mss@gmail.com>
2017-07-21 12:01:43 -0300

    Projeto completo

---
+++ src/calculadora2grau/Calculadora2Grau.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2grau;
+
+import java.lang.Math;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Calculadora2Grau {
+
+   public static Double calculaDelta(String valorX2, String valorX,String valorC){
+       
+       Double x2 = Double.parseDouble(valorX2);
+       Double x = Double.parseDouble(valorX);
+       Double c = Double.parseDouble(valorC);
+       Double delta;
+       
+       
+       delta = Math.pow(x, 2)-4*x2*c;
+       
+       System.out.println(delta);
+       
+       return delta;
+       
+   }
+   
+   
+   public Double calculaX1(String delta, String valorA, String valorB){
+       
+       Double valorDelta = Double.parseDouble(delta);
+       Double B = Double.parseDouble(valorB);
+       Double A = Double.parseDouble(valorA);
+       Double X1;
+       
+       X1 = ((B*-1)+Math.sqrt(valorDelta))/2*A;
+       
+       return X1;
+       
+       
+   }
+   
+   public Double calculaX2(String delta, String valorA, String valorB){
+       
+       Double valorDelta = Double.parseDouble(delta);
+       Double B = Double.parseDouble(valorB);
+       Double A = Double.parseDouble(valorA);
+       Double X1;
+       
+       X1 = ((B*-1)-Math.sqrt(valorDelta))/2*A;
+       
+       return X1;
+       
+       
+   }
+    
+    
+}
